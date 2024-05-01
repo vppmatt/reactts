6bb5863a583a9a8f3cc3f9b7f1d7c708f89f3851
Matt Thornfield <matt@multicode.co.uk>
2024-05-01 16:26:32 +0100

    Update PaymentTableRow.tsx

--- End of day solutions/Day 2/payments-ui/src/components/Transactions/PaymentTableRow.tsx
+++ End of day solutions/Day 2/payments-ui/src/components/Transactions/PaymentTableRow.tsx
@@ -1,14 +1,18 @@
 import { PaymentType } from "../../data/DataFunctions";
 
-const PaymentTableRow = (props: PaymentType) : JSX.Element => {
+const PaymentTableRow = (props: PaymentTableRowProps) : JSX.Element => {
     return <tr>
-        <td>{props.id}</td>
-        <td>{props.orderId}</td>
-        <td>{props.date}</td>
-        <td>{props.country}</td>
-        <td>{props.currency}</td>
-        <td>{props.amount}</td>
+        <td>{props.payment.id}</td>
+        <td>{props.payment.orderId}</td>
+        <td>{props.payment.date}</td>
+        <td>{props.payment.country}</td>
+        <td>{props.payment.currency}</td>
+        <td>{props.payment.amount}</td>
 </tr>;
 };
 
 export default PaymentTableRow;
+
+type PaymentTableRowProps = {
+    payment: PaymentType
+}
